6c50e35b0fb2865d7353c3c75679394fe78249d5
PabloAbreuMinasian <157127354+PabloAbreuMinasian@users.noreply.github.com>
2024-05-12 20:07:39 -0300

    Update SellCar.jsx

--- src/components/views/SellCar.jsx
+++ src/components/views/SellCar.jsx
@@ -3,7 +3,10 @@ import React from "react";
 function SellCar() {
   return (
     <div>
-      <h1>SellCar</h1>
+      <h1>Venda su vehiculo</h1>
+      <div>
+        <div></div>
+      </div>
     </div>
   );
 }
